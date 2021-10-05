bccc17be46ebaabf9975d42d69d100b10f98ccf1
Elia Scherrer <elia.scherrer@trumpf.com>
2021-10-05 09:02:28 +0200

    changed some donwload shit

--- MusicMan - Desktop/MainWindow.xaml.cs
+++ MusicMan - Desktop/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
 
 namespace MusicMan___Desktop
 {
@@ -23,6 +25,75 @@ namespace MusicMan___Desktop
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void DownloadBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string videoUrl = UrlLbl.Text;
+
+            if (string.IsNullOrEmpty(videoUrl))
+                return;
+
+            if (!(new Uri(videoUrl).Host == "www.youtube.com"))
+                return;
+
+            string videoId = RetrieveVideoId(videoUrl);
+
+            if (string.IsNullOrEmpty(videoId))
+                return;
+
+            YoutubeClient youtubeClient = new YoutubeClient();
+            StreamManifest manifest;
+
+            try
+            {
+                manifest = await RetrieveStreamManifest(videoId, youtubeClient);
+            }
+            catch
+            {
+                return;
+            }
+
+            var audioStream = manifest.GetAudioOnlyStreams().GetWithHighestBitrate();
+
+            try
+            {
+                await DonwloadAudio(youtubeClient, audioStream);
+            }
+            catch
+            {
+                return;
+            }
+
+            UrlLbl.Text = "";
+        }
+
+        private string RetrieveVideoId(string videoUrl)
+        {
+            string[] videoParts = videoUrl.Split("/watch?v=");
+            string videoId = "";
+
+            try
+            {
+                videoId = videoParts[1];
+            }
+            catch
+            {
+                return null;
+            }
+
+            return videoId;
+        }
+
+        private async Task<StreamManifest> RetrieveStreamManifest(string videoId, YoutubeClient client)
+        {
+            return await client.Videos.Streams.GetManifestAsync(videoId);
+        }
+
+        private async Task DonwloadAudio(YoutubeClient client, IStreamInfo streamInfo)
+        {
+            string tempPath = "temp.mp3";
+            await client.Videos.Streams.DownloadAsync(streamInfo, tempPath);
         }
     }
 }
